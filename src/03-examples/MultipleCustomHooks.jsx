f758a7114648b3144848c58b1b24a7c7088f0d39
EduEstula1003 <edu.estula@ext.blaze.me>
2022-10-31 18:52:27 -0500

    125. useFecth + useCounter

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -4,9 +4,23 @@ import { useFetch } from '../hooks/useFetch'
 export const MultipleCustomHooks = () => {
 
     const { data, isLoading, hasError } = useFetch("https://www.breakingbadapi.com/api/quotes/1");
-    console.log( data, isLoading, hasError);
+    
+    // const { quote, author } = !!data && data[0];
+    const { quote, author } = !!data ? data[0] : {};
     
     return (
-        <h1>BreakingBad Quotes</h1>
+        <>
+            <h1>BreakingBad Quotes</h1>
+            <hr />
+
+            <div className='alert alert-info text-center'>
+                Loading...
+            </div>
+
+            <blockquote className='blockquote text-end'>
+                <p className='mb-1'>{quote}</p>
+                <footer className='blockquote-footer'>{author}</footer>
+            </blockquote>
+        </>
     )
 }
