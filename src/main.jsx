2d5bb04e012c417e4c66582eb807af3a9acde223
EduEstula1003 <edu.estula@ext.blaze.me>
2022-11-02 18:40:06 -0500

    128. useRef - Primer Uso

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { FormWithCustomHook } from './01-useEffect/FormWithCustomHook';
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
 // import { HooksApp } from './HooksApp';
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MultipleCustomHooks />
+      <FocusScreen />
   // </React.StrictMode>
 )
