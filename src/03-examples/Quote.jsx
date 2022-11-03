ff016744f38fef19284bc13fa62ebc367953a2dc
EduEstula1003 <edu.estula@ext.blaze.me>
2022-11-02 19:02:48 -0500

    129. useLayoutEffect + useRef

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,11 +1,34 @@
+import { useLayoutEffect } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useRef } from "react"
 
 export const Quote = (props) => {
 
+    const pRef = useRef();
+
+    const [boxSize, setBoxSize] = useState({ height: 0, width: 0 });
+
+    useLayoutEffect(() => {
+        const { height, width } = pRef.current.getBoundingClientRect();
+
+        setBoxSize({
+            height,
+            width
+        });
+
+    }, [])
+
 
     return (
-        <blockquote className='blockquote text-end'>
-            <p className='mb-1'>{props.quote}</p>
-            <footer className='blockquote-footer'>{props.author}</footer>
-        </blockquote>
+        <>
+            <blockquote className='blockquote text-end'>
+                <p ref={pRef} className='mb-1'>{props.quote}</p>
+                <footer className='blockquote-footer'>{props.author}</footer>
+            </blockquote>
+
+
+            <code> {JSON.stringify(boxSize)} </code>
+        </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { FormWithCustomHook } from './01-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import { FocusScreen } from './04-useRef/FocusScreen';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+import { Layout } from './05-useLayoutEffect/Layout';
 // import { HooksApp } from './HooksApp';
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-      <FocusScreen />
+      <Layout />
   // </React.StrictMode>
 )
