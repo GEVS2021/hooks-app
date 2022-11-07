57668a718856bcf350d8b21e8b6ac4cdcf836fde
EduEstula1003 <edu.estula@ext.blaze.me>
2022-11-07 18:57:42 -0500

    131. useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MemoHook } from './06-useMemo/MemoHook';
 // import { FormWithCustomHook } from './01-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
-import { Memorize } from './06-useMemo/Memorize';
+// import { Memorize } from './06-useMemo/Memorize';
 // import { HooksApp } from './HooksApp';
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-      <Memorize />
+      <MemoHook />
   // </React.StrictMode>
 )
